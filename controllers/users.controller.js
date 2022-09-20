d07f5de97e67acf1813fece0b8bd230f3ec88359
Stéphane Dauguet <sdauguet@gmail.com>
2022-09-20 18:05:00 +0200

    add try catch

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -3,6 +3,10 @@ const pool = require("../database");
 const { getUsers } = require("../queries/user.queries");
 
 exports.displayHome = async (req, res) => {
-  const results = await getUsers();
-  res.status(200).json({ count: results.rowCount, users: results.rows });
+  try {
+    const results = await getUsers();
+    res.status(200).json({ count: results.rowCount, users: results.rows });
+  } catch (error) {
+    throw error;
+  }
 };
